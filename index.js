5d7c926807a2d22d5264c77abfdd022473e0889a
Abdur Nur Tushar Irfan <tusharctg1202@gmail.com>
2022-12-02 08:25:03 +0600

    mongo and server ready

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//user: frontEnd
+//password: iBWh69CwBj2kapEq
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fpvwzmp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri)
+async function run() {
+    try {
+        const usersCollection = client.db('nexisTask').collection('users')
+        
+
+        //for save users
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result=await usersCollection.insertOne(user)
+            res.send(result)
+
+        })
+
+    }
+    finally {
+
+    }
+} run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Frontend server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Frontend server is running on port ${port}`)
+})
+
